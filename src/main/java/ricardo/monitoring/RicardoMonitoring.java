1be2f5a02b4b14e61b57de12253ea0e3cc3e0a03
Clément MATHIEU <clement@unportant.info>
2019-03-03 19:22:29 +0100

    Initialize Maven and Spring Boot

---
+++ src/main/java/ricardo/monitoring/RicardoMonitoring.java
@@ -0,0 +1,12 @@
+package ricardo.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicardoMonitoring {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicardoMonitoring.class, args);
+	}
+}
